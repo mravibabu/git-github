6d4f89d1f66c5b214a8039a0c47cddede140901a
mravibabu <68624370+mravibabu@users.noreply.github.com>
2020-10-24 14:56:42 +0530

    Add files via upload

---
+++ big_number.py
@@ -0,0 +1,14 @@
+#program to find biggst of the 3 numbers from keyboard
+
+n1=eval(input("enter the first number"))
+n2=eval(input("enter the first number"))
+n3=(eval(input("enter the first number")))	
+
+if n1>n2 and n1>n3:
+
+	print("biggest no is: ",n1)
+elif n2>n3:
+	print("biggest no is:",n2)
+else:
+	print("biggest no is:",n3)
+
